73cae319e74f0f454b4d3a8226daaf0bb44f3fb4
LucaGrilli <luca.grilli@studenti.unicam.it>
2019-05-30 15:55:19 +0200

    inserimento commenti a video per spiegazione funzioni

--- app/controllers/device.controller.ts
+++ app/controllers/device.controller.ts
@@ -12,23 +12,29 @@ export class DeviceController {
 
     @Post()
     async create(@Body() deviceDto: DeviceDto, @Res() res: Response) {
+        console.log("Received POST request for create a new device");
         const response = await this.deviceService.create(deviceDto);
+        console.log(response);
         return res.send(response);
     }
 
     @Delete('/:id')
     async delete(@Param('id') id: number, @Res() res: Response) {
+        console.log("Received DELETE request to eliminate a device");
         const response = await this.deviceService.delete(id);
+        console.log(response);
         return res.send(response);
     }
     
     @Get('/:id')
     async getOneById(@Param('id') id: number): Promise<Device>{
+        console.log("Received GET request to view one device");
         return await this.deviceService.getOneById(id);
     }
 
     @Get()
     async getAll(): Promise<Device[]>{
+        console.log("Received GET request to view all devices");
         return await this.deviceService.getAll();
     }
 }
