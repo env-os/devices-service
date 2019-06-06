c21d5c32dfc6b0d8a2579c9f8eab0423dc529cdf
RiccardoFio <riccardofio97@gmail.com>
2019-06-06 18:00:09 +0200

    Add command

--- app/dto/device.dto.ts
+++ app/dto/device.dto.ts
@@ -1,7 +1,10 @@
+import { Command } from "../entities/command.entity";
+
 export class DeviceDto {
     readonly uuid!: string;
     readonly name!: string;
     readonly macaddress!: string;
     readonly topic!: string;
     readonly description!: string;  
+    readonly commands!: Command[];
 }
